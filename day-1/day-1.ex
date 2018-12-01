6f34e728415ce6f63c0e6501625dc495403e011d
kw7oe <choongkwern@hotmail.com>
2018-12-01 13:09:25 +0800

    Day 1: Part One

---
+++ day-1/day-1.ex
@@ -0,0 +1,9 @@
+case File.read("input.txt") do
+  {:ok, content} ->
+    content
+    |> String.split()
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.sum
+    |> IO.inspect
+  {:error, _} -> IO.puts "Error opening files"
+end
