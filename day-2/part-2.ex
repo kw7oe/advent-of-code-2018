62663cc3175abc98e2a9cb92945bdee92303d4cc
kw7oe <choongkwern@hotmail.com>
2018-12-02 15:11:12 +0800

    Day 2: Part 2

---
+++ day-2/part-2.ex
@@ -0,0 +1,64 @@
+defmodule Solver do
+
+  def solve do
+    case File.read("input.txt") do
+      {:ok, content} ->
+        ids = content |> String.split()
+
+        ids
+        |> Enum.reduce_while([], fn (x, acc) ->
+          rest_of_ids = ids -- [x]
+
+          result = rest_of_ids
+                  |> Enum.reduce_while(:not_found, fn (y, acc) ->
+                    {:x, x}
+                    {:y, y}
+                    case find_common_letter(x, y) do
+                      :not_found -> {:cont, acc}
+                      answer -> {:halt, answer}
+                    end
+                  end)
+                  |> IO.inspect(label: "Result")
+
+
+          case result do
+            :not_found -> {:cont, acc}
+            answer -> {:halt, answer}
+          end
+        end)
+        |> IO.inspect
+
+      {:error, _} -> IO.puts "Error opening file"
+    end
+  end
+
+  def find_common_letter(string1, string2) do
+    graphmes_one = String.graphemes(string1)
+    graphmes_two = String.graphemes(string2)
+
+    differences = graphmes_one
+                  |> Enum.with_index()
+                  |> Enum.filter(fn ({value, index}) ->
+                    value != Enum.at(graphmes_two, index)
+                  end)
+                  |> Enum.map(fn ({value, index}) -> value end)
+
+
+
+    cond do
+      Enum.count(differences) == 1 ->
+        graphmes_two |> IO.inspect
+        graphmes_one |> IO.inspect
+        differences |> IO.inspect
+        (graphmes_one -- differences)
+        |> Enum.join()
+      true -> :not_found
+    end
+  end
+
+end
+
+Solver.solve()
+# Solver.find_common_letter("fgijk", "fgujk") |> IO.inspect
+# Solver.find_common_letter("lsrivfotzbdxpkgnaqmunegchj", "lsrivnotzbdxpkenaqmufrgchj") |> IO.inspect
+
